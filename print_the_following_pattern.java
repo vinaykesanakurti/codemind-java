c3cda49fea53c3f1903fd8d5a6370f4d36616597
vinaykesanakurti <113344616+vinaykesanakurti@users.noreply.github.com>
2022-09-12 15:47:43 +0530

    import java.util.Scanner; class Main { 	public static void main(String args[]) 	{
    		Scanner sc=new Scanner(System.in);
    		int n;
    		n=sc.nextInt();
    		for(int i=1;i<=n;i++)		
    		{   
    			for(int j=1;j<=n;j++)
    			{
    			 if(i==j || i+j==n+1)
    				System.out.print("x");
              else
                 System.out.print("0");
    			}
    			System.out.println();
    		}
    	}
    }

---
+++ print_the_following_pattern.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+class Main
+{
+	public static void main(String args[]) 
+	{
+		
+		Scanner sc=new Scanner(System.in);
+		int n;
+		n=sc.nextInt();
+		for(int i=1;i<=n;i++)		
+		{   
+			for(int j=1;j<=n;j++)
+			{
+			 if(i==j || i+j==n+1)
+				System.out.print("x");
+          else
+             System.out.print("0");
+			}
+			System.out.println();
+		}
+	}
+}
