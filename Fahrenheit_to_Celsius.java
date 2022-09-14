7a026100659e7440af9e7eda2e0b7112ab3d9a71
vinaykesanakurti <113344616+vinaykesanakurti@users.noreply.github.com>
2022-09-14 22:42:48 +0530

    import java.util.*; class main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int c;         float f;         c=sc.nextInt();         f=(5f/9)*(c-32);         System.out.format("%.2f" ,f);         }         }

---
+++ Fahrenheit_to_Celsius.java
@@ -0,0 +1,13 @@
+import java.util.*;
+class main
+{
+    public static void main(String args[])
+    { 
+        Scanner sc=new Scanner(System.in); 
+        int c;
+        float f; 
+        c=sc.nextInt();
+        f=(5f/9)*(c-32);
+        System.out.format("%.2f" ,f);
+        }
+        }
