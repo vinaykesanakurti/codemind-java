990056b59d39a3d86cf4baad83944eab2d3ae033
vinaykesanakurti <113344616+vinaykesanakurti@users.noreply.github.com>
2022-09-14 19:38:39 +0530

    import java.util.Scanner; class Main {     public static void main(String args[])     {         Scanner sc=new Scanner(System.in);         int n,a=0,b=1,c,count=3;         n=sc.nextInt();         System.out.print(a+" "+b+" ");         while(count<=n)         {             c=a+b; a=b; b=c;             System.out.print(c+" ");             count++;             }             sc.close();             }
    }

---
+++ fibonacci.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+class Main
+{
+    public static void main(String args[])
+    {
+        Scanner sc=new Scanner(System.in);
+        int n,a=0,b=1,c,count=3;
+        n=sc.nextInt();
+        System.out.print(a+" "+b+" ");
+        while(count<=n)
+        {
+            c=a+b; a=b; b=c;
+            System.out.print(c+" ");
+            count++;
+            }
+            sc.close();
+            }
+    
+}
